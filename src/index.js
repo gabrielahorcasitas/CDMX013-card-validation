5631f43e40c225a5f5edc75804724fc49bca7662
gabri <gchorcasitas@gmail.com>
2022-07-13 23:45:58 -0500

    avance maquetado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import validator from './validator.js';
+import validator from './validator.js';// para poder importar funcion con default, declarar import en linea 1.
 
 console.log(validator);
+document.getElementById("validation").addEventListener('click', validator);
+
+
+//function numHide (numCard){
+   // hiddenNum = [];
+     //   for (let i = 0; i < numCard.lenght; i ++){
+       //     if (i < numCard.lenght - 4){
+          //      hiddenNum.push("*");
+          //  } else {
+             //   hiddenNum.push(numCard[i]);
+         //   }
+   //     }
+//}
+
+//.value cuando llame a la función con event listenerde typing input en textbox;
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,25 @@
-const validator = {
-  // ...
-};
+const validator = (numCard) => { 
+ let  numT = 0;
+  for (let i = 0; i < numCard.lenght; i ++){
+    let x = numCard [i];
+      if (i < numCard.lenght && i % 2 !== 0){
+        x = x*2;
+        if (x > 9){
+          x = sumDigits(x); 
+        }
+      }
+    numT += x;
+    return numT % 10 === 0;
+    console.log(numT);
+  }
+}
 
-export default validator;
+export default validator; //para poder exportar con deafult una función debe declararse en 1a linea de archivo
+
+function sumDigits (numCard) { //función para sumar dígitos; la usaré para sumar los dígitos de tarjeta cuando sean > 9
+  let convArr = (numCard + '') //convierte los dígitos en array
+  .split ('') //separa los items del array
+  .map(x => parseInt(x)); //devuelve los elementos una vez aplicando parseInt a c/u en un nuevo array
+  convArr [0] + convArr[1];
+  console.log (convArr);
+}
